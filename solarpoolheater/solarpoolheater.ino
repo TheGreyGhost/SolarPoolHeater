3b698f546bcc8654cf83ed592faaff3549c9722a
TheGreyGhost <spamblackhole88@yahoo.com.au>
2018-01-16 00:27:11 +1030

    logging
    Basic logging of temperature probes in place

--- solarpoolheater/solarpoolheater.ino
+++ solarpoolheater/solarpoolheater.ino
@@ -7,10 +7,16 @@
 #include "DataStats.h"
 #include "MovingAverage.h"
 
+#include <SPI.h>
+#include <SD.h>
+#include <Ethernet.h>
+#include <Wire.h>
+#include <RTClib.h>
+
 /********************************************************************/
 // define flags
 const char SPH_VERSION[] = "0.1";
-const bool DEBUG_TEMP = true;
+const bool DEBUG_TEMP = false;
 
 int assertFailureCode = 0;
 #define ASSERT_INVALID_SWITCH 1
@@ -71,7 +77,14 @@ MovingAverage smoothedTemperatures[NUMBER_OF_PROBES] =
 DataStats temperatureDataStats[NUMBER_OF_PROBES];
    
 enum ProbeStatus probeStatuses[NUMBER_OF_PROBES];
-const char* probeNames[] = {"HX_HOT_INLET", "HX_HOT_OUTLET", "HX_COLD_INLET", "HX_COLD_OUTLET"};
+const char* probeNames[NUMBER_OF_PROBES] = {"HX_HOT_INLET", "HX_HOT_OUTLET", "HX_COLD_INLET", "HX_COLD_OUTLET"};
+
+bool echoProbeReadings = false;
+
+const int SD_CHIPSELECT = 4;
+File datalogfile;
+const int DATALOG_BYTES_PER_SAMPLE = NUMBER_OF_PROBES * 3 * sizeof(float); // each sample has min, avg, max for each probe
+const char DATALOG_FILENAME[] = "datalog.txt";
 
 void copyProbeAddress(DeviceAddress dest, DeviceAddress source) {
   int i;
@@ -183,13 +196,102 @@ const char COMMAND_START_CHAR = '!';
 // execute the command encoded in commandString.  Null-terminated
 void executeCommand(char command[]) 
 {
+  bool commandIsValid = false;
   //Serial.print("Execute command:"); Serial.println(command);  
   switch (command[0]) {
-    case 'd': {printDebugInfo(); break;}
-    default: {
-      Serial.print("unknown command:");
-      Serial.println(command);
+    case 'd': {commandIsValid = true; printDebugInfo(); break;}
+    case '?': {
+      commandIsValid = true;
+      Serial.println("commands (turn CR+LF on):");
+      Serial.println("d = print debug info");
+      Serial.println("t = toggle echo of probe readings");
+      Serial.println("le = erase log file");
+      Serial.println("li = log file info");
+      Serial.println("lr sample# count = read log data");
+      break;
     }
+    case 't': {
+      commandIsValid = true; 
+      echoProbeReadings = !echoProbeReadings;
+      break;
+    }
+    case 'l': {
+      switch (command[1]) {
+        case 'e': {             // le erase logfile
+          commandIsValid = true; 
+          bool success;
+          datalogfile.close();
+          success = SD.remove(DATALOG_FILENAME);
+          if (success) {
+            datalogfile = SD.open(DATALOG_FILENAME, FILE_WRITE);
+            if (datalogfile) {
+              Serial.println("data log erased successfully");
+            } else {
+              success = false;
+            }
+          }
+          if (!success) {
+            Serial.println("failed to erase datafile for logged data");
+          }
+          break;
+        }
+        case 'i': { //li file info
+          commandIsValid = true;
+          unsigned long filesize = datalogfile.size();
+          Serial.print(filesize / DATALOG_BYTES_PER_SAMPLE);
+          Serial.println(" samples");
+          break;
+        }
+        case 'r': {    //lr sample# numberOfSamples - read
+          commandIsValid = true;
+          char *nextnumber;
+          long arg1, arg2;
+          arg1 = strtol(command + 2, &nextnumber, 10);
+          arg2 = strtol(nextnumber, &nextnumber, 10);
+          if (arg1 < 0 || arg2 <= 0) {
+            Serial.println("invalid arguments");
+          } else {
+            unsigned long filesize = datalogfile.size();
+            unsigned long samplesInFile = filesize / DATALOG_BYTES_PER_SAMPLE;
+            if (arg1 >= samplesInFile) {
+               Serial.print("arg1 exceeds file size:");
+               Serial.println(samplesInFile);
+            } else {  
+              datalogfile.seek(arg1 * DATALOG_BYTES_PER_SAMPLE);
+              long samplesToRead = min(arg2, min(24*60, samplesInFile - arg1));
+              for (int i = 0; i < NUMBER_OF_PROBES; ++i) {
+                Serial.print(probeNames[i]); 
+                Serial.print(" min avg max ");
+              }
+              Serial.println();
+              for (long i = 0; i < samplesToRead; ++i) {
+                for (int j = 0; j < NUMBER_OF_PROBES; ++j) {
+                  float temp[3];
+                  for (int k = 0; k < sizeof(temp); ++k) {
+                    ((byte *)temp)[k] = datalogfile.read(); 
+                  }
+                  for (int k = 0; k < 3; ++k) {
+                    Serial.print(temp[k], 1); 
+                    Serial.print(" ");
+                  }  
+                }
+                Serial.println();
+              }
+            }
+          }
+          break;
+        }
+      }
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+
+  if (!commandIsValid) {
+    Serial.print("unknown command:");
+    Serial.println(command);
   }
 }
 
@@ -237,6 +339,26 @@ void setup(void)
  Serial.println(success ? "OK" : "ERROR");
  sensors.setResolution(TEMP_11_BIT); //11 bit is 0.125 C
  sensors.setWaitForConversion(false);
+
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(SD_CHIPSELECT)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
+  setupEthernet();
+
+  datalogfile = SD.open(DATALOG_FILENAME, FILE_WRITE);
+
+  if (!datalogfile) {
+    Serial.println("failed to open datafile for writing logged data");
+  }
+
+//  setupRTC();
+
 } 
 
 unsigned long temperatureSampleMillis = 0;
@@ -273,7 +395,7 @@ void loop(void)
         }
       }
 
-      if (DEBUG_TEMP) {
+      if (echoProbeReadings) {
         Serial.print(probeNames[i]);
         Serial.print(":");
         if (probeStatuses[i] == OK) {
@@ -287,7 +409,7 @@ void loop(void)
             default: Serial.println("Invalid status"); break;
           }
         }
-      }  // DEBUG_TEMP
+      }  // echoProbeReadings
     }  // for i = 0 to NUMBER_OF_PROBES
     unreadTemperatures = false;
   } else if (timeNow - temperatureSampleMillis >= TEMP_SAMPLE_PERIOD) { // start next conversion
@@ -303,5 +425,171 @@ void loop(void)
   processIncomingSerial();
   /********************************************************************/
 
+  if (temperatureDataStats[0].getCount() >= 10) {
+//    loopEthernet();
+//    loopRTC();
+    String dataString = "test";
+
+    // if the file is available, write to it:
+    if (datalogfile) {  // don't forget to update DATALOG_BYTES_PER_SAMPLE
+      Serial.print("start datafile write:"); Serial.println(millis());
+      for (int i = 0; i < NUMBER_OF_PROBES; ++i) {
+        float temp[3];
+        if (probeStatuses[i] == OK) {
+          temp[0] = temperatureDataStats[i].getMin();
+          temp[1] = temperatureDataStats[i].getAverage();
+          temp[2] = temperatureDataStats[i].getMax();
+        } else {
+          temp[0] = 0.0; temp[1] = 0.0; temp[2] = 0.0;  
+        }
+        datalogfile.write((byte *)temp, sizeof temp);
+        temperatureDataStats[i].clear();
+      }
+      Serial.print("start datafile flush:"); Serial.println(millis());
+      datalogfile.flush();
+      Serial.print("end datafile flush:"); Serial.println(millis());
+      Serial.print("file size:");
+      Serial.println(datalogfile.size());
+    }
+  }
 }
 
+/*
+  Web Server
+
+ A simple web server that shows the value of the analog input pins.
+ using an Arduino Wiznet Ethernet shield.
+
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13
+ * Analog inputs attached to pins A0 through A5 (optional)
+
+ created 18 Dec 2009
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe
+ modified 02 Sept 2015
+ by Arturo Guadalupi
+
+ */
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 1, 177);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetServer server(80);
+
+void setupEthernet() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+}
+
+
+void loopEthernet() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");
+          }
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
+
+void printDateTime(DateTime dateTime);
+
+RTC_DS1307 RTC;
+
+void setupRTC(void){
+  Serial.begin(9600);
+  Wire.begin();
+  RTC.begin();
+}
+
+void loopRTC() {
+  if (Serial.available() > 0) {
+  int instruct = Serial.read();
+  switch (instruct) {
+    case 'D': {
+      DateTime now = RTC.now();
+      printDateTime(now);
+      break;
+    } case 'S':
+      RTC.set(RTC_MONTH, 6);
+      RTC.set(RTC_HOUR, 16);
+      break;
+    }
+  }
+}
+
+void printDateTime(DateTime dateTime) {
+  Serial.print(dateTime.year(), DEC);
+  Serial.print('/');
+  Serial.print(dateTime.month(), DEC);
+  Serial.print('/');
+  Serial.print(dateTime.day(), DEC);
+  Serial.print(' ');
+  Serial.print(dateTime.hour(), DEC);
+  Serial.print(':');
+  Serial.print(dateTime.minute(), DEC);
+  Serial.print(':');
+  Serial.print(dateTime.second(), DEC);
+  Serial.println();
+}
