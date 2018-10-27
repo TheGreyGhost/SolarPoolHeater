89e4b8d8c2c27478f0b400679ae112d4d346f11d
TheGreyGhost <5291905+TheGreyGhost@users.noreply.github.com>
2018-10-27 10:46:33 +1030

    minor tweak

--- solarpoolheater/TemperatureProbes.cpp
+++ solarpoolheater/TemperatureProbes.cpp
@@ -1,13 +1,17 @@
 #include "TemperatureProbes.h"
 #include <OneWire.h> 
+
+#define REQUIRESALARMS false // Don't need the temp probe alarms
 #include <DallasTemperatureErrorCodes.h>
 #include "Simulate.h"
-#include "PinAssignments.h"
 
+/********************************************************************/
+// Data wire is plugged into pin 2 on the Arduino 
+#define TEMP_PROBES_ONE_WIRE_BUS 2 
 /********************************************************************/
 // Setup a oneWire instance to communicate with any OneWire devices  
 // (not just Maxim/Dallas temperature ICs) 
-OneWire oneWire(DIGPIN_TEMP_PROBES_ONE_WIRE_BUS); 
+OneWire oneWire(TEMP_PROBES_ONE_WIRE_BUS); 
 /********************************************************************/
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperatureErrorCodes sensors(&oneWire);
@@ -28,8 +32,6 @@ const float MIN_PLAUSIBLE_TEMPERATURE = -5.0;
 const float MAX_PLAUSIBLE_TEMPERATURE = 120.0;
 
 const int16_t INITIAL_VALUE_RAW = 0x2A80;  // this is the default powerup value; 85.0 C.  If we ever see it, just ignore it.
-//const float INITIAL_VALUE_CELCIUS = 85.0;  // this is the default powerup value; 85.0 C.  If we ever see it, just ignore it.  Shouldn't be necessary; todo remove bugfix
-//const float INITIAL_VALUE_CELCIUS_TOLERANCE = 0.1;  // this is the default powerup value; 85.0 C.  If we ever see it, just ignore it.
 
 DeviceAddress probeAddresses[NUMBER_OF_PROBES] = 
    {{0x28, 0xFF, 0xCC, 0x47, 0x71, 0x17, 0x03, 0x09},  // HX_HOT_INLET
@@ -183,10 +185,8 @@ void tickTemperatureProbes()
           case DEVICE_NOT_FOUND: probeStatuses[i] = PS_NOT_FOUND; ++errorCountNotFound[i]; break;
           default: assertFailureCode = ASSERT_INVALID_SWITCH; probeStatuses[i] = PS_NOT_FOUND; ++errorCountNotFound[i]; break; 
         }
-      } else if (tempValue == INITIAL_VALUE_RAW) { // || fabs(tempValueCelcius - INITIAL_VALUE_CELCIUS) < INITIAL_VALUE_CELCIUS_TOLERANCE ) {
+      } else if (tempValue == INITIAL_VALUE_RAW) { // ignore the reading during probe initialisation
           probeStatuses[i] = PS_OK;
-//          lastdudtempcelcius = tempValueCelcius;
-//          lastdudtemp = tempValue;
       } else  {
         if (tempValueCelcius < MIN_PLAUSIBLE_TEMPERATURE || tempValueCelcius > MAX_PLAUSIBLE_TEMPERATURE) {
           probeStatuses[i] = PS_IMPLAUSIBLE_VALUE;
@@ -227,13 +227,9 @@ void tickTemperatureProbes()
     bool success = sensors.requestTemperatures(); // Send the command to get temperature readings 
     temperatureSampleMillis = timeNow;
     unreadTemperatures = true;
-
-//            console->print(lastdudtemp, HEX);  //todo remove these
-//          console->print(":");
-//          console->println(lastdudtempcelcius);
-//          // don't store anything
-
   }
 
 
 }
+
+
