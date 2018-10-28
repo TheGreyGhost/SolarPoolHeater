d2cd1a9202d3eab7d894a1522e1ad1397a568b31
TheGreyGhost <5291905+TheGreyGhost@users.noreply.github.com>
2018-10-29 00:23:24 +1030

    Adding Ethernet as parallel to Serial

--- solarpoolheater/OutputDestination.cpp
+++ solarpoolheater/OutputDestination.cpp
@@ -14,3 +14,63 @@ size_t OutputDestinationSerial::write(const uint8_t *buf, size_t size)
 {
   return Serial.write(buf, size);
 }
+
+OutputDestinationEthernet::OutputDestinationEthernet(EthernetUDP &udpConnection, IPAddress remoteIP, unsigned int remotePort)
+ : m_udpConnection(udpConnection), m_remoteIP(remoteIP), m_remotePort(remotePort)
+{
+}
+
+void OutputDestinationEthernet::begin()
+{
+  // nothing required since we already supplied an initialised connection
+  m_queuedBytes = 0;
+}
+
+size_t OutputDestinationEthernet::write(uint8_t b)
+{
+  return write(&b, 1);
+}
+
+size_t OutputDestinationEthernet::write(const uint8_t *buf, size_t size)
+{
+  const MAX_PACKET_SIZE = 500;
+  size_t bytesSent = 0;
+
+// keep sending chunks until we have less than the MAX packet size remaining
+  while (m_queuedBytes + (size - bytesSent) >= MAX_PACKET_SIZE) {
+    size_t bytesToAdd = MAX_PACKET_SIZE - m_queuedBytes;
+    addChunk(buf + bytesSent, bytesToAdd, true);
+    bytesSent += bytesToAdd;
+  }
+
+// find the last \n
+  for (int i = size; i > bytesSent && buf[i-1] != '\n'; --i);
+  
+  if (i > bytesSent) {  // found one
+    size_t bytesToAdd = i - bytesSent;
+    addChunk(buf + bytesSent, bytesToAdd, true);
+    bytesSent += bytesToAdd;
+  }
+  if (bytesSent < size) {
+    addChunk(buf + bytesSent, bytesToAdd, false);
+  }
+  return size;
+}
+
+// add a chunk to the packet.  Start a new packet if necessary, end the packet if flush is true
+size_t OutputDestinationEthernet::addChunk(const uint8_t *buf, size_t size, bool flush)
+{
+  if (m_queuedBytes == 0) {
+    m_udpConnection.beginPacket(m_remoteIP, m_remotePort);
+  }
+  m_udpConnection.write(buf, size);
+  if (flush) {
+    m_udpConnection.endPacket();     
+    m_queuedBytes = 0;
+  } else {
+    m_queuedBytes += size;
+  }  
+  return size;
+}
+
+
--- solarpoolheater/OutputDestination.h
+++ solarpoolheater/OutputDestination.h
@@ -10,4 +10,20 @@ public:
   using Print::write;
 };
 
+
+// will wait until \n is received before sending packet; or until packet size reaches 500 bytes
+class OutputDestinationEthernet : public Print {
+public:
+  OutputDestinationEthernet(EthernetUDP &udpConnection, IPAddress remoteIP, unsigned int remotePort);
+  virtual void begin();
+  virtual size_t write(uint8_t);
+  virtual size_t write(const uint8_t *buf, size_t size);
+  using Print::write;
+private:
+  EthernetUDP &m_udpConnection;
+  unsigned int m_remotePort;
+  IPAddress m_remoteIP;
+  int m_queuedBytes;
+};
+
 #endif
